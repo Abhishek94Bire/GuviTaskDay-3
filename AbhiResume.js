1e3de92d8592c3e5038425f3a01ead960a4c93e7
Abhishek94Bire <137614575+Abhishek94Bire@users.noreply.github.com>
2023-09-27 09:08:50 +0530

    Add files via upload

---
+++ AbhiResume.js
@@ -0,0 +1,63 @@
+//2. Create your own Resume using JSON format
+
+let resume = {
+"1. Basics": {
+    "Name": "Abhishek Bire",
+    "email": "Abhibire94@gmail.com",
+    "Phone no.": 9834549254,
+    "Location": "Nagpur, India",
+    },
+"2. Profiles": [ 
+    {
+    "linkedIn": "linkedin.com/in/abhishek-bire-77357a257/",
+    "GitHub": "github.com/Abhishek94bire",
+    }
+],
+"3. Education": [
+    {
+    "1. UG Degree": "B.E in Mechanical Engineering, YOP-2017",
+    "2. Diploma/12th": "ACS Colleage Tukum YOP-2012",
+    
+    }
+],
+"4. Skills": [
+    {
+    "Teamwork, Adaptability, Decision Making": 5,
+    "JavaScript, C/C++, Verilog": 3.5,
+    "LABview, OptiSystem, Keil uvision": 4,
+    }
+],
+"5. Languages Known": [
+    {
+    "List": "English, Marathi,",
+    }
+],
+"6. Certifications": [
+    {
+    1: "GUVI-JavaScript, valid from 2022",
+    
+    }
+],
+"7. Projects": [
+    {
+    1: "Title: Performance Analysis of Underwater Wireless Optical Communication system with MIMO",
+    2: "Description: Designed a UWOC system with MIMO to achieve improvement in link range in a high turbulence water medium.",
+    3: "Duration: 4 months",
+    4: "Title: Pulse Oximetry System using IoT",
+    5: "Description: Designed a cost effective real time remote monitoring pulse oximetry system using ESP8266 and IoT.",
+    6: "Duration: 3 months",
+    
+    }
+],
+"8. Extracurricular":[
+    {
+    1: "EWIP College Level Badminton Tournament 2017 (Runners Up), 2016 (Runners Up).",
+    2: "Inter Schools Volleyball Tournament 2010 (Winners).",
+    }
+] 
+}
+
+console.log(resume);
+
+
+
